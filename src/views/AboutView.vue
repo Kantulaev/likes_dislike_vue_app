f84c943dd90b45355b0d23dcf1a0a5e5d8b9a64d
Kantulaev <kantulaev@gmail.com>
2022-05-05 11:14:39 +0300

    Likes_Dislikes_app_1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page 123</h1>
+    <h2>Лайков: {{likes}}</h2>
+    <h2>Дизлайков: {{dislikes}}</h2>
+    <button v-on:click="LikesUp">Like</button>
+    <button @click = "DislikesUp">Dislike</button>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      likes: 0,
+      dislikes: 0
+    }
+  },
+  
+  methods: {
+    LikesUp(){
+      this.likes += 1;
+    },
+    DislikesUp(){
+      this.dislikes += 1;
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
